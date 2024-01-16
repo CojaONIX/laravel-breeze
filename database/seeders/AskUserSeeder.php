37d5b90ad481d357f64f9dcbea4cc10a19f35adc
CojaONIX <cojaonix@gmail.com>
2024-01-16 10:27:40 +0100

    homework 11 - AskUserSeeder

--- database/seeders/AskUserSeeder.php
+++ database/seeders/AskUserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class AskUserSeeder extends Seeder
 {
@@ -12,6 +14,40 @@ class AskUserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $console = $this->command->getOutput();
+        $email = $console->ask("Unesite email korisnika?");
+        if($email === null)
+        {
+            $console->error("Niste uneli email korisnika!");
+            die;
+        }
+
+        if(User::where(['email' => $email])->first() !== null)
+        {
+            $console->error("U bazi vec postoji korisnik $email!");
+            die;
+        }
+
+        $name = $console->ask("Unesite naziv korisnika?");
+        if($name === null)
+        {
+            $console->error("Niste uneli naziv korisnika!");
+            die;
+        }
+
+        $password = $console->ask("Unesite password korisnika?");
+        if($password === null)
+        {
+            $console->error("Niste uneli password korisnika!");
+            die;
+        }
+
+        User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => Hash::make($password)
+        ]);
+        $console->success("Uspesno ste uneli novog korisnika!");
+        $console->table(['Name', 'Email', 'Password'], [['email' => $email, 'name' => $name, 'password' => $password]]);
     }
 }
